2b46d9d8bf141d1a75d5435f213c2a9a48af297e
Ramkrishna goud <ramakrishnagoud9199@gmail.com>
2025-07-22 22:07:11 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // Note: Tailwind is usually used via PostCSS and doesn't need to be here.
-    // If you're using Tailwind through PostCSS config, you can remove this line.
-  ],
+  plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/student-deploy",
 });
