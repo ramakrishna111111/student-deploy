2ee381af89f344c4aa3772fbccdcaa07040d13a4
Ramkrishna goud <ramakrishnagoud9199@gmail.com>
2025-07-22 21:59:34 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    base : process.env.VITE_BASE_PATH || "/student-deploy"
 
   ],
 })
