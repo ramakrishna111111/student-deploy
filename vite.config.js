a6ed6f9cb11758dcfe14595fd0cec324f1000866
Ramkrishna goud <ramakrishnagoud9199@gmail.com>
2025-07-22 22:20:52 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/student-deploy/",
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/student-deploy",
 });
